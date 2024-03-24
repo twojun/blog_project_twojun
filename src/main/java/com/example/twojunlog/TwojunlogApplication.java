e67350ddd312775e179db146017b152d68d0baba
twojun <twojunspace@naver.com>
2024-03-24 18:13:01 +0900

    Initial commit

---
+++ src/main/java/com/example/twojunlog/TwojunlogApplication.java
@@ -0,0 +1,12 @@
+package com.example.twojunlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojunlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwojunlogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/twojunlog/TwojunlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twojunlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwojunlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
